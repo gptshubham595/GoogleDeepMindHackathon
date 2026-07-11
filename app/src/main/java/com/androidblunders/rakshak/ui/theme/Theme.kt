c80badeef7bf70f3d755ed1c83ba9066b1e9e9fb
gptshubham595 <gptshubham595@gmail.com>
2026-07-11 16:14:17 +0530

    integration tts stt fixes

--- app/src/main/java/com/androidblunders/rakshak/ui/theme/Theme.kt
+++ app/src/main/java/com/androidblunders/rakshak/ui/theme/Theme.kt
@@ -59,9 +59,9 @@ fun RakshakTheme(
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            val window = (view.context as? Activity)?.window
+            window?.statusBarColor = colorScheme.primary.toArgb()
+            window?.let { WindowCompat.getInsetsController(it, view).isAppearanceLightStatusBars = darkTheme }
         }
     }
 
